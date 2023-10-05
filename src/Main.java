ba3e4a3f8fe1500501bfd0e8ec101c81b73c757b
K3n <kalyankaushik.khasibhatla@gmail.com>
2023-10-05 21:36:05 +0530

    Added some content - checking

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,30 @@ public class Main {
     public static void main(String[] args) {
        Shape shape = new Shape();
 
-       Point point = new Point(1,3);
+       Point point = new Point(1,3, 4);
        Polygon polygon = new Polygon(point);
+
+       var i = 9;
+       var j = 8;
+
+
+        System.out.println(shape.toString() + i);
+        System.out.println(polygon.toString() + j);
+        System.out.println(ret(point));
+    }
+
+
+    private static int ret(Point p){
+
+        return switch (p.x()){
+            case 1 -> 1;
+            case 2 -> 2;
+            case 3 -> {
+                System.out.println("hello");
+                yield 4;
+            }
+            default -> 0;
+        };
+
     }
 }
